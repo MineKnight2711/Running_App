79b20123c08bea9cd5c23d3a5f93bad9764783ed
MineKnight2711 <133186183+MineKnight2711@users.noreply.github.com>
2024-05-30 15:28:12 +0700

    fix preparation botton sheet

--- lib/screens/preparation/components/bottom_sheet/components/preparation_bottom_sheet.dart
+++ lib/screens/preparation/components/bottom_sheet/components/preparation_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_running_demo/widgets/custom_draggable_sheet/custom_draggable_sheet.dart';
 import '../../../../../config/config_export.dart';
 import 'package:flutter_running_demo/controllers/map_controller.dart';
 import 'package:get/get.dart';
@@ -25,63 +24,128 @@ class PreparationBottomSheet extends StatelessWidget {
       'Add-new',
       'Upcoming',
     ];
-    return CustomDraggableSheet(
-      child: (context, scrollController) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                height: 5,
-                width: 70.0,
-                decoration: BoxDecoration(
-                    color: Colors.grey[400],
-                    borderRadius: BorderRadius.circular(10.0)),
+    return DraggableScrollableSheet(
+      initialChildSize: 0.25,
+      maxChildSize: 0.9,
+      minChildSize: 0.25,
+      builder: (context, scrollController) {
+        return SingleChildScrollView(
+          controller: scrollController,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: AppColors.sheetBackground,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24),
+                topRight: Radius.circular(24),
               ),
             ),
-            PreparationTabbar(
-              listButton: preTypes,
-              tabController: tabController,
-              onSelectedIndex: onSelectedIndex,
-            ),
-            SizedBox(height: AppSpacings.vs10),
-            const SheetGrabberTitle(),
-            SizedBox(height: AppSpacings.vs5),
-            SizedBox(
-              height: Get.height * 0.66,
-              child: TabBarView(
-                controller: tabController,
-                children: [
-                  RoutePreparationList(
-                    scrollController: scrollController,
-                    routes: tempTopRoute,
+            padding: EdgeInsets.symmetric(horizontal: AppSpacings.hs15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    height: 5,
+                    width: 70.0,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(10.0)),
                   ),
-                  Obx(() {
-                    return mapController.selectedRoute.value == null
-                        ? const AddNewRouteActionGuideWidget()
-                        : SingleChildScrollView(
-                            child: RouteItemWidget(
-                              showDivider: false,
-                              route: mapController.selectedRoute.value!,
-                              isSelected: true,
-                              isSelectedWidget: const AddNewRouteItemOption(),
-                            ),
-                          );
-                  }),
-                  UpcomingListRoute(
-                    scrollController: scrollController,
-                    anyTimeRoutes: tempTopRoute.sublist(0, 2),
-                    byTimeRoutes: tempTopRoute.sublist(
-                        tempTopRoute.length - 1, tempTopRoute.length),
+                ),
+                PreparationTabbar(
+                  listButton: preTypes,
+                  tabController: tabController,
+                  onSelectedIndex: onSelectedIndex,
+                ),
+                SizedBox(height: AppSpacings.vs10),
+                const SheetGrabberTitle(),
+                SizedBox(height: AppSpacings.vs5),
+                SizedBox(
+                  height: Get.height * 0.705,
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      RoutePreparationList(
+                        scrollController: scrollController,
+                        routes: tempTopRoute,
+                      ),
+                      Obx(() {
+                        return mapController.selectedRoute.value == null
+                            ? const AddNewRouteActionGuideWidget()
+                            : SingleChildScrollView(
+                                child: RouteItemWidget(
+                                  showDivider: false,
+                                  route: mapController.selectedRoute.value!,
+                                  isSelected: true,
+                                  isSelectedWidget:
+                                      const AddNewRouteItemOption(),
+                                ),
+                              );
+                      }),
+                      UpcomingListRoute(
+                        scrollController: scrollController,
+                        anyTimeRoutes: tempTopRoute.sublist(0, 2),
+                        byTimeRoutes: tempTopRoute.sublist(
+                            tempTopRoute.length - 1, tempTopRoute.length),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         );
       },
     );
+
+    //     CustomDraggableSheetWidget(
+    //   inititalSize: 0.25,
+    //   maxSize: 0.85,
+    //   minSize: 0.25,
+    //   grabberBottom: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       PreparationTabbar(
+    //         listButton: preTypes,
+    //         tabController: tabController,
+    //         onSelectedIndex: onSelectedIndex,
+    //       ),
+    //       SizedBox(height: AppSpacings.vs10),
+    //       const SheetGrabberTitle(),
+    //       SizedBox(height: AppSpacings.vs5),
+    //     ],
+    //   ),
+    //   sheetBodyBuilder: (context, scrollController) {
+    //     return TabBarView(
+    //       controller: tabController,
+    //       children: [
+    //         RoutePreparationList(
+    //           scrollController: scrollController,
+    //           routes: tempTopRoute,
+    //         ),
+    //         Obx(() {
+    //           return mapController.selectedRoute.value == null
+    //               ? const AddNewRouteActionGuideWidget()
+    //               : SingleChildScrollView(
+    //                   child: RouteItemWidget(
+    //                     showDivider: false,
+    //                     route: mapController.selectedRoute.value!,
+    //                     isSelected: true,
+    //                     isSelectedWidget: const AddNewRouteItemOption(),
+    //                   ),
+    //                 );
+    //         }),
+    //         UpcomingListRoute(
+    //           scrollController: scrollController,
+    //           anyTimeRoutes: tempTopRoute.sublist(0, 2),
+    //           byTimeRoutes: tempTopRoute.sublist(
+    //               tempTopRoute.length - 1, tempTopRoute.length),
+    //         ),
+    //       ],
+    //     );
+    //   },
+    // );
   }
 }
